f0f79b2fc4cfde69412949d388b4ad682c71a016
chandraprakashsingh281@gmail.com <chandraprakashsingh00000@gmail.com>
2026-01-31 14:33:45 +0530

    first commit

--- server/src/Middleware/visitor.js
+++ server/src/Middleware/visitor.js
@@ -1,26 +1,32 @@
 // middleware/visitor.js
-const { v4: uuidv4 } = require('uuid');
-const cookieParser = require('cookie-parser');
+const { v4: uuidv4 } = require("uuid");
+const cookieParser = require("cookie-parser");
 
-const COOKIE_NAME = 'visitorId';
-const COOKIE_MAX_AGE = Number(process.env.COOKIE_MAX_AGE_MS) || 1000 * 60 * 60 * 24 * 365;
+const COOKIE_NAME = "visitorId";
+const COOKIE_MAX_AGE =
+  Number(process.env.COOKIE_MAX_AGE_MS) ||
+  1000 * 60 * 60 * 24 * 365;
 
 function visitorMiddleware(req, res, next) {
-  // cookie-parser must be used before this middleware in app
-  let vid = req.cookies && req.cookies[COOKIE_NAME];
+  // cookie-parser must be used before this middleware
+  const vid = req.cookies?.[COOKIE_NAME];
 
   if (!vid) {
-    vid = `v_${uuidv4()}`;
-    const isProd = process.env.NODE_ENV === 'production';
-    res.cookie(COOKIE_NAME, vid, {
-      httpOnly: false,      // set to true if you don't need client JS to read it
-      secure: isProd,       // true in prod (HTTPS)
-      sameSite: 'lax',
-      maxAge: COOKIE_MAX_AGE
+    const newVid = `v_${uuidv4()}`;
+    const isProd = process.env.NODE_ENV === "production";
+
+    res.cookie(COOKIE_NAME, newVid, {
+      httpOnly: false,
+      secure: isProd,
+      sameSite: "lax",
+      maxAge: COOKIE_MAX_AGE,
     });
+
+    req.visitorId = newVid;
+  } else {
+    req.visitorId = vid;
   }
 
-  req.visitorId = vid;
   next();
 }
 
